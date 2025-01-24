3e0c104b358f9d24162862a144adec43e0deb390
brosenmoller <rosenmollerben@gmail.com>
2025-01-25 00:11:33 +0100

    Bubble spawner iteration

--- Assets/Player/Scrips/PlayerFreezeController.cs
+++ Assets/Player/Scrips/PlayerFreezeController.cs
@@ -3,14 +3,16 @@ using UnityEngine.InputSystem;
 
 public class PlayerFreezeController : MonoBehaviour
 {
-    private void Start() {
+    private void Start()
+    {
         InputManager.Instance.Controls.Gameplay.Freeze.performed += PlayerFreezeAction;
     }
 
-    public void PlayerFreezeAction(InputAction.CallbackContext callbackContext) {
-        Debug.Log("Freeze");
+    public void PlayerFreezeAction(InputAction.CallbackContext callbackContext)
+    {
         BubbleController[] freezables = FindObjectsByType<BubbleController>(FindObjectsSortMode.None);
-        foreach (BubbleController controller in freezables) { 
+        foreach (BubbleController controller in freezables)
+        { 
             controller.Freeze();
         }
     }
