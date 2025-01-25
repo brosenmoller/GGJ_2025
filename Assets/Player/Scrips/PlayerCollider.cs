6ff6a726229a8284991897b4f7c2efba44426bc8
Mia Vreven <miavreven@gmail.com>
2025-01-25 04:14:53 +0100

    Add Temp death particle

--- Assets/Player/Scrips/PlayerCollider.cs
+++ Assets/Player/Scrips/PlayerCollider.cs
@@ -1,13 +1,38 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class PlayerCollider : MonoBehaviour
 {
+    PlayerMovement playerMovement;
+    new Rigidbody2D rigidbody2D;
+
+    [SerializeField]
+    private ParticleSystem deathParticle;
+
+    private void Awake()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("DeathTrigger"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            playerMovement.enabled = false;
+            rigidbody2D.linearVelocity = Vector2.zero;
+            rigidbody2D.gravityScale = 0;
+            deathParticle.Play();
+            StartCoroutine(DelayedReload());
         }
     }
+
+    private IEnumerator DelayedReload()
+    {
+        yield return new WaitForSeconds(0.76f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
 }
